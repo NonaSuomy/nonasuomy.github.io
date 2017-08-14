f8d136516f7ec21c4ed521732fd3e2aaad73f77f
NonaSuomy <nonasuomy@live.com>
2017-08-14 12:10:36 -0400

    Update place-ads.rb

--- _plugins/place-ads.rb
+++ _plugins/place-ads.rb
@@ -7,7 +7,18 @@ module Jekyll
     end
 
     def render(context)
-      "#{@text}"
+      "<div>
+       <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+       <ins class="adsbygoogle"
+         style="display:block; text-align:center;"
+         data-ad-format="fluid"
+         data-ad-layout="in-article"
+         data-ad-client="ca-pub-6522065888169039"
+         data-ad-slot="8755974884"></ins>
+       <script>
+         (adsbygoogle = window.adsbygoogle || []).push({});
+      </script>
+      </div>"
     end
   end
 end
